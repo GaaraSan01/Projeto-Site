65d1e068b8b128dbcaa9c7cd397bd00d5d886a5a
VniciDigital <91447833+GaaraSan01@users.noreply.github.com>
2022-05-06 00:29:03 -0300

    Melhorias
    Resolução de erros

--- TabNavigation.js
+++ TabNavigation.js
@@ -58,31 +58,3 @@ window.addEventListener('load', () =>{
     const tabNavigation = TabNavigation()
     tabNavigation.init()
 })
-
-const ValorInicial = $("#valor-investid")
-const Tempo = $("#temp")
-const Resultado = $("#res")
-
-function OcultarRes(){
-    Resultado.style.display = "none"
-}
-OcultarRes()
-function Calcular(){
-    const Valor = Number(ValorInicial.value)
-    const tempo = Number(Tempo.value)
-    const juros = 1.9/100
-    if(Valor >= 1000 && tempo >= 12){
-        Resultado.style.display = "block"
-        const montante = Valor *(1 + juros)** tempo
-        Resultado.innerHTML = `<p> Investindo 
-        ${Valor.toLocaleString('pt-BR',{style:'currency',currency:'BRL'})}
-        com o prazo de ${tempo} meses, no total você terá 
-        ${montante.toLocaleString('pt-BR',{style:'currency',currency:'BRL'})}. </p>`
-    } else{
-        window.alert('ERRO! Preencha os dados Corretamente!')
-        OcultarRes()
-    }
-    $("#valor-investid").value = ''
-    $("#valor-investid").focus()
-    $("#temp").value = ''
-}
--- script.js
+++ script.js
@@ -1,22 +1,27 @@
-let valor = document.getElementById('valorinv')
-let prazo = document.getElementById('prazoinv');
-let res = document.getElementById('res');
-let rentabilidade = 1.8;
+const ValorInicial = $("#valor-investid")
+const Tempo = $("#temp")
+const Resultado = $("#res")
 
-function simular(){
-     let val = Number (valor.value);
-     let praz = Number (prazo.value);
-     let juros = Number(rentabilidade/100);
-
-     if(val >= 1000 && praz >= 12 ){
-         let montante = val*(1 + juros)**praz
-         res.innerHTML = `<p> Investindo ${val.toLocaleString('pt-BR',{style:'currency',currency:'BRL'})} no prazo de ${praz} meses
-            você estará com ${montante.toLocaleString('pt-BR',{style:'currency',currency:'BRL'})}.</p>`;
-     } else{
-            window.alert('ERRO!!');
-        }
-valorinv.value = ''
-valorinv.focus()
- prazoinv.value = ''
-prazoinv.focus()
- }
+function OcultarRes(){
+    Resultado.style.display = "none"
+}
+OcultarRes()
+function Calcular(){
+    const Valor = Number(ValorInicial.value)
+    const tempo = Number(Tempo.value)
+    const juros = 1.9/100
+    if(Valor >= 1000 && tempo >= 12){
+        Resultado.style.display = "block"
+        const montante = Valor *(1 + juros)** tempo
+        Resultado.innerHTML = `<p> Investindo 
+        ${Valor.toLocaleString('pt-BR',{style:'currency',currency:'BRL'})}
+        com o prazo de ${tempo} meses, no total você terá 
+        ${montante.toLocaleString('pt-BR',{style:'currency',currency:'BRL'})}. </p>`
+    } else{
+        window.alert('ERRO! Preencha os dados Corretamente!')
+        OcultarRes()
+    }
+    $("#valor-investid").value = ''
+    $("#valor-investid").focus()
+    $("#temp").value = ''
+}
